8b7712f2e814351701289f8282095e25c8adee15
klina8575 <klina8575@gmail.com>
2021-07-23 15:53:55 +0900

    modifiy

--- js/lookup.js
+++ js/lookup.js
@@ -1,7 +1,15 @@
 
 $(function(){
     $(".btn01").click(function(){ 
-        alert("예약정보: 인원은 총 2명이며 파리의 오전 컨셉을 예약하셨습니다.");
-        location.href = 'lookup.html';
+
+        var phone = $('#phone').val();
+
+        if(phone) {
+            alert("예약정보: 인원은 총 2명이며 파리의 오전 컨셉을 예약하셨습니다.");
+            location.href = 'lookup.html';
+        } else {
+            alert("전화번호 입력을 확인해주세요!")
+        }
+
     });
 });
--- js/reservation.js
+++ js/reservation.js
@@ -1,8 +1,25 @@
 
 $(function(){
     $(".btn01").click(function(){ 
-        alert("예약이 완료되었습니다.");
-        location.href = 'reservation.html';
+        var people = $('#people').val();
+        var phone = $('#phone').val();
+        var chk = false;
+
+
+        $("input:checkbox[name=reservation_chk]").each(function() {
+            if($(this).is(":checked") == true){
+                console.log('체크된 상태');
+                chk = true;
+            }
+        });
+
+
+        if(people && phone && chk) {
+            alert("예약이 완료되었습니다.");
+            location.href = 'reservation.html';
+        } else {
+            alert("예약정보 입력을 확인해주세요!")
+        }
     });
 });
 
